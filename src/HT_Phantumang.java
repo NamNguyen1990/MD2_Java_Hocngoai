9bf3a7c4f768d7a9bdf215ec28bb8fe6771f7ab2
NAM <namxuan.nguyen1990@gmail.com>
2022-04-04 15:25:52 +0700

    HT_PTumang

---
+++ src/HT_Phantumang.java
@@ -0,0 +1,6 @@
+public class HT_Phantumang {
+public static void main(String[] args) {
+    int[] a = new int[] {5,5,9,10};
+    System.out.println("Hiển thị số ở vị trí thứ 2 trong mảng là:" + a[2]);
+}
+}
