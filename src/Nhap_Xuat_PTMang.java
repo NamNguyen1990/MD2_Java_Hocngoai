88f44b28113895ce40d7d1058ea9e324434489e9
NAM <namxuan.nguyen1990@gmail.com>
2022-04-04 16:34:29 +0700

    Nhap_Xuat_

---
+++ src/Nhap_Xuat_PTMang.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Nhap_Xuat_PTMang {
+    public static void main(String[] args) {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số lượng phần tử mảng");
+        size = scanner.nextInt();
+        int[] mang = new int[size];
+//      vòng lặp này sẽ duyệt i từ 0 đến chiều dài của mảng - 1
+        for (int i=0; i<size; i++) {
+            System.out.println("Nhập vào phần tử thứ" + i + ":");
+            mang[i] = scanner.nextInt(); // Nhập phần tử cho mảng
+        }
+        for (int i=0; i<size; i++) {
+            System.out.println(mang[i]); // In ra mảng nhập vào
+        }
+    }
+}
