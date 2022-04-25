a6fbbab1aabae159b8b363457611c1dd8f306597
NamNguyen1990 <namxuan.nguyen1990@gmail.com>
2022-04-26 02:43:18 +0700

    Read_sd_Scaner

--- src/WriteData.java
+++ src/WriteData.java
@@ -5,7 +5,7 @@ public class WriteData {
     public static void main(String[] args) throws IOException, FileNotFoundException {
         java.io.File file = new java.io.File("text10.txt");  // tạo file mới vào truyền Hello vào
         if (file.exists()) {
-            System.out.println("File already exists");
+            System.out.println("File đã tồn tại");
             System.exit(0);
         }
         java.io.PrintWriter output = new java.io.PrintWriter(file);
