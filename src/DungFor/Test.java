58c98167edecfcdfd89a3ee14d81c66eb8ac5556
NamNguyen1990 <namxuan.nguyen1990@gmail.com>
2022-04-26 01:42:45 +0700

    taoFilevaghiFile

--- src/DungFor/Test.java
+++ src/DungFor/Test.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        String[] abc = {"abc", "cbd", "1", "2"};
-        for(int i = 0; i < abc.length; i++) {
-            System.out.println(abc[i]);
-        };
-        System.out.println("----------------");
-        ArrayList<Double> abc1 = new ArrayList<>();
-        for(Double i : abc1) {
-            System.out.println(i);
+
+        int[] mang = new int[5];
+        try {
+            mang[6] = 12;
         }
+        catch (IndexOutOfBoundsException a) {
+            System.out.println(a.getMessage());
+        }
+        catch (Exception b) {
+            System.out.println("Loi vuot qua chi so cho phep: " + b.getMessage());
+        }
+
+
+//        String[] abc = {"abc", "cbd", "1", "2"};
+//        for(int i = 0; i < abc.length; i++) {
+//            System.out.println(abc[i]);
+//        };
+//        System.out.println("----------------");
+//        ArrayList<Double> abc1 = new ArrayList<>();
+//        for(Double i : abc1) {
+//            System.out.println(i);
+//        }
     }
 }
+
--- src/TestIO.java
+++ src/TestIO.java
@@ -3,14 +3,21 @@ import java.io.IOException;
 
 public class TestIO {
     public static void main(String[] args) throws IOException {
-        File file = new File("text5.txt");
-        if (file.isFile()) {
-            file.delete();
-            System.out.println("Đây là File Text" + file.getAbsolutePath());
+        File file = new File("text1.txt");
+        if (file.exists()) {
+            System.out.println("Đã tồn tại File" + file.getAbsolutePath());
+//            file.delete();
+            System.exit(0);
         }
         else {
-            System.out.println("Đây không không phải là File");
+            System.out.println("Chưa tồn tại File");
             file.createNewFile();
         }
+        java.io.PrintWriter output = new java.io.PrintWriter(file);  // Chỉ ghi khi file chưa tồn tại
+
+
+        output.print("Hello");
+        output.close();
+
     }
 }
