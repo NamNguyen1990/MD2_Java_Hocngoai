723f01012f82ae7df07b5da66cb0a92a849fc3c0
NAM <namxuan.nguyen1990@gmail.com>
2022-04-26 08:24:08 +0700

    na commit

--- src/TestIO.java
+++ src/TestIO.java
@@ -3,14 +3,15 @@ import java.io.IOException;
 
 public class TestIO {
     public static void main(String[] args) throws IOException {
-        File file = new File("text5.txt");
+        File file = new File("D:\\CodeGym\\Modul 2_1\\Java\\Hocngoai\\Text1.txt");
         if (file.isFile()) {
-            file.delete();
+//            file.delete();
             System.out.println("Đây là File Text" + file.getAbsolutePath());
         }
         else {
             System.out.println("Đây không không phải là File");
             file.createNewFile();
+
         }
     }
 }
