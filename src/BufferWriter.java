31b667d35c0a2b5eabb40f4df757c5dd274154ba
NAM <namxuan.nguyen1990@gmail.com>
2022-04-26 10:38:49 +0700

    Buffer

--- src/BufferWriter.java
+++ src/BufferWriter.java
@@ -1,24 +1,30 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
+import java.io.FileWriter;
 
 public class BufferWriter {
-    public static void main(String[] args) {
-        String tx = "Hello Word";
-        FileInputStream fileInputStream = null;
-        BufferedInputStream bufferedInputStream = null;
+    public static void main(String[] args) throws IOException {
+        String txt = "Hello Word";
         try {
-            fileInputStream = new FileInputStream("text5.text");
-            bufferedInputStream = new BufferedInputStream(fileInputStream);
-
-
-
+            FileWriter fw = new FileWriter("data.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(txt);
+            bw.close();
+            fw.close();
 
         }catch (Exception e) {
-
-        } finally {
-
         }
 
+        FileReader fr = new FileReader("text2.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String s = "";
+        while (true) {
+            s = br.readLine();
+            if (s == null) {
+                break;
+            }
+            System.out.println(s);
+        }
+        br.close();
+        fr.close();
     }
 }
